79ff9e4691594377fd7b768e40a789d2d9c2e39e
Nae Cosmin <cosminnae00@yahoo.com>
2022-05-03 15:53:55 +0300

    fetch data when load

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 // Map
 
-const map = L.map('map').setView([37.38605, -122.08385], 12);
-const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-});
-osm.addTo(map);
+const coordinates = (lat, lng) => {
+  const map = L.map('map').setView([lat, lng], 12);
+  const osm = L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  );
+  osm.addTo(map);
 
-const marker = L.marker([37.38605, -122.08385]).addTo(map);
+  const marker = L.marker([lat, lng]).addTo(map);
+};
 
 // API
 
-const test = async () => {
-  const response = await fetch(
-    'https://geo.ipify.org/api/v2/country,city?apiKey=at_pir6vPHSrwk58xItmUfF79suaWhcy&ipAddress=8.8.8.8'
-  );
+const ipText = document.querySelector('.ip');
+const city = document.querySelector('.city');
+const timezone = document.querySelector('.timezone');
+const isp = document.querySelector('.isp');
+let ip = '';
 
+const getIP = async () => {
+  const response = await fetch('https://api.ipify.org?format=json');
   const data = await response.json();
-  console.log(data.location);
+  ip = data.ip;
+
+  getDataOnLoad(ip);
 };
 
-test();
+const getDataOnLoad = async (ip) => {
+  const response = await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_pir6vPHSrwk58xItmUfF79suaWhcy&ipAddress=${ip}`
+  );
+  const data = await response.json();
 
-// https://geo.ipify.org/docs --- API Page
+  let lat = data.location.lat;
+  let lng = data.location.lng;
+
+  console.log(data);
+  console.log(data.location.city);
+  ipText.innerHTML = data.ip;
+  city.innerHTML = `${data.location.city}, ${data.location.region}`;
+  timezone.innerHTML = data.location.timezone;
+  isp.innerHTML = data.isp;
+
+  coordinates(lat, lng);
+};
+
+getIP();
